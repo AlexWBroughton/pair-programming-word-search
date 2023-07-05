94571227d293df9ca22b50b343966ca91ea181d0
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-05 20:22:43 +0000

    wordsearch app that allows for horizontal, vertical and backwards functionality.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    let reversedArray = word.split('');
+    reversedArray = reversedArray.reverse();
+ 
+    const reversedWord = reversedArray.join('');
+
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const transposedLetters = letters[0].map((col,i) => letters.map(row=>row[i]));
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
+    for (l of verticalJoin) {
+         if (l.includes(word)) return true;
+    }
+    for (l of horizontalJoin) {
+      if (l.includes(reversedWord)) return true;
+    }
+    for (l of verticalJoin) {
+       if (l.includes(reversedWord)) return true;
+    }
+    return false;
+  }
+
+const wordArray = [
+  ['a','b','c','d'],
+  ['c','f','g','h'],
+  ['e','z','s','t']
+]
+console.log(wordSearch(wordArray,""));
 
 module.exports = wordSearch
+
+
+//const numRows = letters.length;
+//const numCols = letters[0].length;
+//
+
